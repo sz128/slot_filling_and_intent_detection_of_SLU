424b0b83d9718502e248b3da2d94115214b8d79a
sz128 <paul2204@qq.com>
2018-08-21 16:13:43 +0800

    Update run_slot_tagging.sh

--- run/run_slot_tagging.sh
+++ run/run_slot_tagging.sh
@@ -18,4 +18,6 @@ bs=16
 me=100
 mwf=1 #If you want to replace rare word with <UNK>, please set $mwf large than 1.
 
+# Set deviceId=-1 if you are going to use cpu for training.
+
 python3 scripts/slot_tagging.py --task $task --dataset $dataset --dataroot $dataroot --bidirectional --lr $lr --dropout $dp --batchSize $bs --optim $opt --max_norm $mn --experiment exp --deviceId 0 --max_epoch $me --emb_size $es --hidden_size $hs --mini_word_freq $mwf 
