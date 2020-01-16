ad0c0d16e708a4969e07b1ae8ab7c638aea41e59
Su Zhu <zhusu.china@gmail.com>
2020-01-16 15:41:26 +0800

    add ECSA dataset

---
+++ run/ECSA_with_pure_bert.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source ./path.sh
+
+task_slot_filling=$1 # NN, NN_crf
+task_intent_detection=$2 # CLS, max, CLS_max
+balance_weight=1 #0.5
+
+pretrained_model_type=bert
+pretrained_model_name=bert-base-chinese #bert-base-uncased #bert-large-uncased-whole-word-masking #bert-base-uncased
+
+dataroot=data/ECSA
+dataset=ECSA
+
+batch_size=64 # 16, 32
+
+optimizer=bertadam #bertadam, adamw, adam, sgd
+learning_rate=5e-5 # 1e-5, 5e-5, 1e-4, 1e-3
+max_norm_of_gradient_clip=1 # working for adamw, adam, sgd
+dropout_rate=0.1 # 0.1, 0.5
+
+max_epoch=30
+
+device=0
+# device=0 means auto-choosing a GPU
+# Set deviceId=-1 if you are going to use cpu for training.
+experiment_output_path=exp
+
+python scripts/slot_tagging_and_intent_detection_with_pure_transformer.py --task_st $task_slot_filling --task_sc $task_intent_detection --dataset $dataset --dataroot $dataroot --lr $learning_rate --dropout $dropout_rate --batchSize $batch_size --optim $optimizer --max_norm $max_norm_of_gradient_clip --experiment $experiment_output_path --deviceId $device --max_epoch $max_epoch --st_weight ${balance_weight} --pretrained_model_type ${pretrained_model_type} --pretrained_model_name ${pretrained_model_name}
